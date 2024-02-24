be2c206d38be2d2b269889e78944111414789f1c
ulnvkrll <ulnvkrll@gmail.com>
2024-02-24 22:09:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задание 1 //
+        float coins = 5;
+        float cassa = coins / 2;
+
+        System.out.println("В каждой кассе лежит - " + cassa);
+
+        // Задание 2 //
+        int a = 50;
+        int b = 20;
+        int s = a * b;
+
+        System.out.println("Площадь квадрата - " + s);
+
+        // Задание 3 //
+        int hungry = 9;
+        int full = 15;
+        int bananas = 3;
+        int time1 = full/bananas;
+        int time2 = hungry/bananas;
+        int dif = time1 - time2;
+
+        System.out.println("Голодный Вася съедает бананы быстре на - " + dif);
+
+        // Задание 4 //
+        /* public class Application {
+           public static void main(String[] args) { */
+                int a = 3;
+                int a = 10;
+                int b = 6;
+                int b = 12;
+                int sum = 1 + a + b;
+                int sum = 2 + a + b;
+                int sum = 3 + a + b;
+                int sum = 4 + a + b;
+
+                System.out.println("sum = " + sum);
+           // }
+       // }
+    }
+}
